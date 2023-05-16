0516cfd62528b0ab7725679f90acbfb4e4a73698
Brajesh Kumar <chauhan.cpt@gmail.com>
2023-05-16 15:11:56 +0530

    removed border

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className="container border">
+    <div className="container">
       <h2 className={styles.description}>Hello World!!!</h2>
     </div>
   )
