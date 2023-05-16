2f1af8b9a40bdb143e626ad920cd3852ecce864b
Brajesh Kumar <chauhan.cpt@gmail.com>
2023-05-16 14:20:53 +0530

    working on demo project

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
-      <h2 className={styles.description}>Hello demo projects</h2>
+    <div className="container border">
+      <h2 className={styles.description}>Hello World</h2>
     </div>
   )
 }
